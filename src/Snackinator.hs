2e91336a4b98be25b23a6162462c2c3e722e8915
Jamie Bertram <jamie@novoideation.ca>
2019-07-12 08:17:07 -0400

    Load snackers and products

--- src/Snackinator.hs
+++ src/Snackinator.hs
@@ -1,19 +1,24 @@
+{-# LANGUAGE ApplicativeDo #-}
 {-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 module Snackinator
     ( snackMain
     ) where
 
 import Control.Monad
+import Control.Monad.Except
 import Data.Aeson
 import Data.Aeson.Casing
 import Data.Bifunctor
+import Debug.Trace
 import GHC.Generics (Generic)
-import Network.HTTP
-import Network.Stream
-import Network.URI (parseURI)
+import Network.HTTP.Req
 
-import qualified Data.ByteString.Lazy as B
+import qualified Data.ByteString.Lazy as BL
+import qualified Data.ByteString.Lazy.UTF8 as BU
+import qualified Data.ByteString as B
 
 
 data Gender
@@ -33,23 +38,49 @@ data Snacker = Snacker
     }
     deriving (Eq, Ord, Show, Generic)
 
+data Variant = Variant
+    { variantId :: Int
+    , variantPrice :: String
+    }
+    deriving (Eq, Ord, Show, Generic)
+
+data Product = Product
+    { productId :: Int
+    , productTitle :: String
+    , productTags :: [String]
+    , productVariants :: [Variant]
+    }
+    deriving (Eq, Ord, Show, Generic)
+
+newtype Products = Products { products :: [Product] } deriving (Eq, Ord, Show, Generic)
 
 instance FromJSON Gender
 instance FromJSON Snacker where
     parseJSON = genericParseJSON $ aesonPrefix snakeCase
+instance FromJSON Variant where
+    parseJSON = genericParseJSON $ aesonPrefix snakeCase
+instance FromJSON Product where
+    parseJSON = genericParseJSON $ aesonPrefix snakeCase
+instance FromJSON Products
 
 snackMain :: IO ()
-snackMain = print =<< (getJsonDoc snackerUrl :: IO (Either String [Snacker]))
+snackMain = do
+    p <- runExceptT $ do
+        snackers <- getSnackers
+        products <- getProducts
+        pure products
+    print p
 
-snackerUrl :: String
-snackerUrl = "http://s3.amazonaws.com/misc-file-snack/MOCK_SNACKER_DATA.json"
+getSnackers :: ExceptT String IO [Snacker]
+getSnackers = getJsonDoc (https "s3.amazonaws.com" /: "misc-file-snack" /: "MOCK_SNACKER_DATA.json") mempty
 
-getJsonDoc :: FromJSON a => String -> IO (Either String a)
-getJsonDoc = fmap (eitherDecode . rspBody <=< first show) . simpleHTTP . getBsRequest
+getProducts :: ExceptT String IO Products
+getProducts = getJsonDoc (https "ca.desknibbles.com" /: "products.json") $
+    "limit" =: (250 :: Int)
+    <> header "User-Agent" "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:68.0) Gecko/20100101 Firefox/68.0"
 
-getBsRequest :: String -> Request B.ByteString
-getBsRequest urlString =
-    case parseURI urlString of
-      Nothing -> error ("getRequest: Not a valid URL - " ++ urlString)
-      Just u  -> mkRequest GET u
-  
+getJsonDoc :: FromJSON a => Url Https -> Option Https -> ExceptT String IO a
+getJsonDoc url opts = ExceptT
+    . fmap (eitherDecode . (\a -> trace (show a) a) . responseBody)
+    . runReq defaultHttpConfig
+    $ req GET url NoReqBody lbsResponse opts
