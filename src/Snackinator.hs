f510e11bbc605e1bbb31997c691fc40b267535bc
Jamie Bertram <jamie@novoideation.ca>
2019-07-12 09:08:57 -0400

    Calculate fave snacks

--- src/Snackinator.hs
+++ src/Snackinator.hs
@@ -7,19 +7,68 @@ module Snackinator
     ( snackMain
     ) where
 
+import Control.Arrow
 import Control.Monad
 import Control.Monad.Except
 import Data.Aeson
 import Data.Aeson.Casing
 import Data.Bifunctor
+import Data.List
+import Data.Maybe
 import Debug.Trace
 import GHC.Generics (Generic)
 import Network.HTTP.Req
+import Text.Read
 
 import qualified Data.ByteString.Lazy as BL
 import qualified Data.ByteString.Lazy.UTF8 as BU
 import qualified Data.ByteString as B
 
+snackMain :: IO ()
+snackMain = do
+    result <- runExceptT $ do
+        snackers <- getSnackers
+        products <- getProducts
+        pure
+            $ map ((fst . head) &&& map snd)
+            $ groupBy
+                (\x y -> productId (fst x) == productId (fst y))
+                [(p, s) | p <- products,
+                          s <- snackers,
+                          snackerFaveSnack s == productTitle p ]
+    case result of
+        Left e -> error e
+        Right snackerSnacks -> do
+            putStrLn ""
+            putStrLn "Real snacks found:"
+            forM_ snackerSnacks $ \(product, snackers) ->
+                putStrLn $
+                    "  "
+                    ++ productTitle product
+                    ++ " (fave snack of: "
+                    ++ intercalate ", " (map snackerEmail snackers)
+                    ++ ")"
+            putStrLn ""
+            let snacksFlat = concatMap (\(product, snackers) -> map (const product) snackers) snackerSnacks
+            putStrLn $
+                "If all "
+                ++ show (length snacksFlat)
+                ++ " snackers bought their fave snack, in total it would cost $"
+                ++ show (sum $ mapMaybe (readMaybe :: String -> Maybe Double) $ concatMap (map variantPrice . productVariants) snacksFlat)
+
+getSnackers :: ExceptT String IO [Snacker]
+getSnackers = getJsonDoc (https "s3.amazonaws.com" /: "misc-file-snack" /: "MOCK_SNACKER_DATA.json") mempty
+
+getProducts :: ExceptT String IO [Product]
+getProducts = fmap products $ getJsonDoc (https "ca.desknibbles.com" /: "products.json") $
+    "limit" =: (250 :: Int)
+    <> header "User-Agent" "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:68.0) Gecko/20100101 Firefox/68.0"
+
+getJsonDoc :: FromJSON a => Url Https -> Option Https -> ExceptT String IO a
+getJsonDoc url opts = ExceptT
+    . fmap (eitherDecode . responseBody)
+    . runReq defaultHttpConfig
+    $ req GET url NoReqBody lbsResponse opts
 
 data Gender
     = Male
@@ -62,25 +111,3 @@ instance FromJSON Variant where
 instance FromJSON Product where
     parseJSON = genericParseJSON $ aesonPrefix snakeCase
 instance FromJSON Products
-
-snackMain :: IO ()
-snackMain = do
-    p <- runExceptT $ do
-        snackers <- getSnackers
-        products <- getProducts
-        pure products
-    print p
-
-getSnackers :: ExceptT String IO [Snacker]
-getSnackers = getJsonDoc (https "s3.amazonaws.com" /: "misc-file-snack" /: "MOCK_SNACKER_DATA.json") mempty
-
-getProducts :: ExceptT String IO Products
-getProducts = getJsonDoc (https "ca.desknibbles.com" /: "products.json") $
-    "limit" =: (250 :: Int)
-    <> header "User-Agent" "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:68.0) Gecko/20100101 Firefox/68.0"
-
-getJsonDoc :: FromJSON a => Url Https -> Option Https -> ExceptT String IO a
-getJsonDoc url opts = ExceptT
-    . fmap (eitherDecode . (\a -> trace (show a) a) . responseBody)
-    . runReq defaultHttpConfig
-    $ req GET url NoReqBody lbsResponse opts
